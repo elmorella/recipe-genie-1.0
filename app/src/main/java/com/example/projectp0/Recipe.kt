84503e0a5939909af0ba3425ccf1c57a4ae69734
Elmer Orellana <104410892+ssblue18@users.noreply.github.com>
2022-07-30 19:59:10 -0700

    remove network client interface; update entity with parcelable

--- app/src/main/java/com/example/projectp0/Recipe.kt
+++ app/src/main/java/com/example/projectp0/Recipe.kt
@@ -1,15 +1,53 @@
 package com.example.projectp0
 
+import android.os.Parcel
+import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.google.gson.JsonArray
 
 @Entity(tableName = "recipes")
-data class Recipe(@PrimaryKey(autoGenerate = true) var recipeId: Int?,
-                  @ColumnInfo(name = "title") var title: String,
-                  @ColumnInfo(name = "rYield") var rYield: String,
-                  @ColumnInfo(name = "prepTime") var prepTime: String,
-                  @ColumnInfo(name = "totalTime") var totalTime: String,
-                  @ColumnInfo(name = "ingredients") var ingredients: String,
-                  @ColumnInfo(name = "directions") var directions: String)
+data class Recipe(
+    @PrimaryKey(autoGenerate = true) var recipeId: Int?,
+    @ColumnInfo(name = "title") var title: String?,
+    @ColumnInfo(name = "rYield") var rYield: String?,
+    @ColumnInfo(name = "prepTime") var prepTime: String?,
+    @ColumnInfo(name = "totalTime") var totalTime: String?,
+    @ColumnInfo(name = "ingredients") var ingredients: String?,
+    @ColumnInfo(name = "directions") var directions: String?
+    ) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readValue(Int::class.java.classLoader) as? Int,
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeValue(recipeId)
+        parcel.writeString(title)
+        parcel.writeString(rYield)
+        parcel.writeString(prepTime)
+        parcel.writeString(totalTime)
+        parcel.writeString(ingredients)
+        parcel.writeString(directions)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Recipe> {
+        override fun createFromParcel(parcel: Parcel): Recipe {
+            return Recipe(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Recipe?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
